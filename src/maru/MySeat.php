700830b9cdd6064692e558ebb7e4dc16490efb6c
maru <wsj7178@gmail.com>
2016-01-16 20:37:18 +0900

    error fix

--- src/maru/MySeat.php
+++ src/maru/MySeat.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace maru;
 
 use pocketmine\plugin\PluginBase;
@@ -6,56 +7,58 @@ use pocketmine\event\Listener;
 use ifteam\SuperUser\SuperUser;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerPreLoginEvent;
+
 class MySeat extends PluginBase implements Listener {
 	/**
-	 * 
+	 *
 	 * @var SuperUser
 	 */
 	private $su;
 	public function onEnable() {
-		$this->su = $this->getServer()->getPluginManager()->getPlugin("SuperUser");
-		if($this->su == null) {
-			$this->getLogger()->error("SuperUser 플러그인을 찾을 수 없습니다.");
-			$this->getServer()->getPluginManager()->disablePlugin($this);
+		$this->su = $this->getServer ()->getPluginManager ()->getPlugin ( "SuperUser" );
+		if ($this->su == null) {
+			$this->getLogger ()->error ( "SuperUser 플러그인을 찾을 수 없습니다." );
+			$this->getServer ()->getPluginManager ()->disablePlugin ( $this );
 		}
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
 	public function onPlayerJoin(PlayerPreLoginEvent $event) {
-		if($this->CountOnlinePlayers() >= $this->getServer()->getMaxPlayers() - $this->CountManagers()) {
-			$player = $event->getPlayer();
-			if(!$this->isManager($player)) {
-				$event->setKickMessage("서버가 꽉 찼습니다!");
-				$event->setCancelled();
+		if ($this->CountOnlinePlayers () >= $this->getServer ()->getMaxPlayers () - $this->CountManagers ()) {
+			$player = $event->getPlayer ();
+			if (! $this->isManager ( $player )) {
+				$event->setKickMessage ( "서버가 꽉 찼습니다!" );
+				$event->setCancelled ();
 			}
 		}
 	}
 	private function CountManagers() {
 		$count = 0;
-		foreach($this->su->db as $v) {
-			$count++;
+		foreach ( $this->su->db as $v ) {
+			$count ++;
 		}
 		return $count;
 	}
 	private function CountOnlinePlayers() {
 		$count = 0;
-		foreach($this->getServer()->getOnlinePlayers() as $player) {
-			$count++;
+		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {
+			$count ++;
 		}
 		return $count;
 	}
 	/**
-	 * 
-	 * @param Player|string $player
+	 *
+	 * @param Player|string $player        	
 	 * @return boolean
 	 */
 	private function isManager($player) {
-		if($player instanceof Player) {
-			$player = $player->getName();
+		if ($player instanceof Player) {
+			$player = $player->getName ();
 		}
-		foreach($this->su->db as $su) {
-			foreach($su as $pass) {
-				foreach ($pass as $name) {
-					return true;
+		foreach ( $this->su->db as $su ) {
+			foreach ( $su as $pass ) {
+				foreach ( $pass as $name ) {
+					if ($name == $player)
+						return true;
 				}
 			}
 		}
