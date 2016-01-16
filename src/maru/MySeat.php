0b1a9b80f9e7818b791369e878e685aff215228f
hm <hmkuak@naver.com>
2016-01-16 21:10:21 +0900

    a few changes...

--- src/maru/MySeat.php
+++ src/maru/MySeat.php
@@ -5,7 +5,7 @@ namespace maru;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use ifteam\SuperUser\SuperUser;
-use pocketmine\Player;
+use pocketmine\IPlayer;
 use pocketmine\event\player\PlayerPreLoginEvent;
 
 class MySeat extends PluginBase implements Listener {
@@ -14,54 +14,52 @@ class MySeat extends PluginBase implements Listener {
 	 * @var SuperUser
 	 */
 	private $su;
+	private $suList;
 	public function onEnable() {
 		$this->su = $this->getServer ()->getPluginManager ()->getPlugin ( "SuperUser" );
-		if ($this->su == null) {
+		if ($this->su === null) {
 			$this->getLogger ()->error ( "SuperUser 플러그인을 찾을 수 없습니다." );
 			$this->getServer ()->getPluginManager ()->disablePlugin ( $this );
+			return;
 		}
+		
+		$this->getSuList ();
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
 	public function onPlayerJoin(PlayerPreLoginEvent $event) {
-		if ($this->CountOnlinePlayers () >= $this->getServer ()->getMaxPlayers () - $this->CountManagers ()) {
-			$player = $event->getPlayer ();
-			if (! $this->isManager ( $player )) {
-				$event->setKickMessage ( "서버가 꽉 찼습니다!" );
-				$event->setCancelled ();
+		if (count ( $this->getServer ()->getOnlinePlayers () ) < $this->getServer ()->getMaxPlayers ())
+			return;
+		
+		if ($this->isManager ( $event->getPlayer () ))
+			$event->setCancelled ();
+	}
+	private function getSuList() {
+		if (isset ( $this->su->db ['su'] ))
+			foreach ( $this->su->db ['su'] as $su ) {
+				foreach ( $su as $passkey => $data ) {
+					if ($data ['firstLoginName'] !== null)
+						$this->suList [$data ['firstLoginName']] = true;
+				}
 			}
-		}
-	}
-	private function CountManagers() {
-		$count = 0;
-		foreach ( $this->su->db as $v ) {
-			$count ++;
-		}
-		return $count;
-	}
-	private function CountOnlinePlayers() {
-		$count = 0;
-		foreach ( $this->getServer ()->getOnlinePlayers () as $player ) {
-			$count ++;
-		}
-		return $count;
+		if (isset ( $this->su->db ['staff'] ))
+			foreach ( $this->su->db ['staff'] as $staff ) {
+				foreach ( $staff as $passkey => $data ) {
+					$this->suList [$data ['firstLoginName']] = true;
+				}
+			}
 	}
 	/**
 	 *
-	 * @param Player|string $player        	
+	 * @param IPlayer|string $player        	
 	 * @return boolean
 	 */
 	private function isManager($player) {
-		if ($player instanceof Player) {
-			$player = $player->getName ();
-		}
-		foreach ( $this->su->db as $su ) {
-			foreach ( $su as $pass ) {
-				foreach ( $pass as $name ) {
-					if ($name == $player)
-						return true;
-				}
-			}
-		}
+		if (! $player instanceof IPlayer)
+			return false;
+		
+		if (isset ( $this->suList [$player->getName ()] ))
+			return true;
+		
 		return false;
 	}
 }
